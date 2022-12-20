75c103f38d0d0666777283e71f4edd69fc613991
ninjadiego <diego3c34@gmail.com>
2022-12-19 19:42:23 -0500

    mensaje consola

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
+	fmt.Println("Hello, 世界")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-// Fin codigo
-// segundo comentario
